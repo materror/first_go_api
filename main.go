a1ad07516b68ff93913576cc5ce2236e0c9e0140
materror <matatiesa123@gmail.com>
2025-07-01 16:19:02 +0200

    Extremely basic successful API GET call for todo list

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"golang.org/x/net/html"
-	"strings"
+	//"fmt"
+	//"golang.org/x/net/html"
+	//"encoding/json"
+	"net/http"
+	"github.com/gin-gonic/gin"
 )
-// Goal: make a website API that allows the user to parse HTMLs
+
+type todo struct {
+	ID uint `json:"id"`
+	Item string `json:"item"`
+	Description string `json:"desc"`
+	Completed bool `json:"status"`
+}
+var todo_list = []todo{
+	{ID: 1, Item: "Pasutit EVAK karti", Description: "Ieej eveseliba.gov.lv, vajag lidz Portugalei paspet", Completed: false},
+	{ID: 2, Item: "MethodsX paper", Description: "Izej cauri tam kas ir izdarits, velreiz parlasi example, sac rakstit", Completed: false},
+	{ID: 3, Item: "Facebook marketplace salikt mebeles", Description: "Var ari pielikt taja wpp grupa, pajauta Tomas", Completed: false},
+}
+
+func getTodoList(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todo_list)
+}
+
+// Goal: make a website API that works as a to-do list
 func main() {
-	htmlData := ""
-
-	// Parse HTML string
-	doc, err := html.Parse(strings.NewReader(htmlData))
-	if err != nil {
-		fmt.Println("Error parsing HTML:", err)
-		return
-	}
-
-	fmt.Printf("%v", doc)
+	router := gin.Default()
+	router.GET("/todo_list", getTodoList)
+	router.Run("localhost:9090")
+	
 }
